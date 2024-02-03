14ae4bb7465ea8d1b8a48354244146f583f5c97c
TrucDev <truc.huynh@austincc.edu>
2024-02-03 17:45:41 -0600

    Update hello.php
    add content

--- hello.php
+++ hello.php
@@ -1,1 +1,15 @@
+<?php
+/* This is a block comment in PHP
+   which spans multiple lines.
+   You can use it to explain complex logic or
+   to temporarily disable blocks of code.
+*/
 
+echo "This line is executable code."; // This line will be executed
+
+/* Task;
+  - Write PHP code that outputs "Hello, World!" to the browser.
+ */
+
+echo "??";
+?>
