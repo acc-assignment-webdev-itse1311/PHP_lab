313fb3d434318a319474612800fd03754a9ba6ed
TrucDev <truc.huynh@austincc.edu>
2024-02-03 17:51:36 -0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/* This is a block comment in PHP
+   which spans multiple lines.
+   You can use it to explain complex logic or
+   to temporarily disable blocks of code.
+*/
+
+echo "This line is executable code."; // This line will be executed
+
+/*
+  Objective: Learn how to create and use functions in PHP.
+  Task:
+  - Write a function that takes a name as an argument and returns a greeting, e.g., "Hello, [name]!".
+  - Call the function with a few different names and echo the results.
+  Expected Outcome: The script outputs a personalized greeting for each name provided to the function.
+ */
+
+echo "??";
+?>
