c742d8ce0a3ce3143e6dcf7a6b29c4c324917b18
TrucDev <truc.huynh@austincc.edu>
2024-02-03 17:53:39 -0600

    Update control-structures.php

--- control-structures.php
+++ control-structures.php
@@ -7,13 +7,12 @@
 
 echo "This line is executable code."; // This line will be executed
 
-/*Objective: Understand how to use if-else statements and loops in PHP.
-  Task:
-  - Create a PHP file named control-structures.php.
-  - Use an if-else statement to check if a number is positive or negative.
-  - Use a for loop to print numbers from 1 to 10.
-  Expected Outcome: 
-  - The script correctly identifies a number as positive or negative and prints numbers from 1 to 10.
+/*
+   Objective: Understand how to use if-else statements and loops in PHP.
+   Task:
+   - Use an if-else statement to check if a number is positive or negative.
+   - Use a for loop to print numbers from 1 to 10.
+   Expected Outcome: The script correctly identifies a number as positive or negative and prints numbers from 1 to 10.
  */
 
 echo "??";
