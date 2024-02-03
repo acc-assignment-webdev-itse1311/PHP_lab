0a35f9158773831c8d43039b2df4c1ef6ef98df4
TrucDev <truc.huynh@austincc.edu>
2024-02-03 17:46:52 -0600

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+/* This is a block comment in PHP
+   which spans multiple lines.
+   You can use it to explain complex logic or
+   to temporarily disable blocks of code.
+*/
+
+echo "This line is executable code."; // This line will be executed
+
+/* Task:
+  - Declare two variables, one for your name and another for your age.
+  - Echo a sentence that includes both variables, e.g., "My name is John, and I am 30 years old."
+  Expected Outcome: The browser displays a sentence incorporating the values stored in the variables.
+ */
+
+echo "??";
+?>
